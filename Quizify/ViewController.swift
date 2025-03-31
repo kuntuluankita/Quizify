17e21784f7f444ebdf030a4359ffaebf32a3d216
Kuntulu Ankita <kuntuluankita@gmail.com>
2025-03-31 22:10:34 +0530

    Initial Commit

---
+++ Quizify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizify
+//
+//  Created by Kuntulu Ankita on 31/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
